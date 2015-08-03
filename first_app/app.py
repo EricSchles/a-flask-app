8084a9ff47dcc7259cc9d221f7295b2ca2e44ef4
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-03 11:14:35 -0700

    dynamic behavior example

--- first_app/app.py
+++ first_app/app.py
@@ -3,12 +3,13 @@
 from flask import Flask 
 
 app = Flask(__name__)
+guesses = ['Python', 'Java', 'C++']
 
 @app.route('/')
 def index():
 	return '<h1>Guess The Language!</h1>'
-
-@app.route('/guess/<init:id>')
+ 
+@app.route('/guess/<int:id>')
 def guess(id):
 	return('<h1>Guess The Language!</h1>'
 			'<p>My guess: {0}</p>').format(guesses[id])
