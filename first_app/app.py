5ba3e422c7fcd4f37e31076271066b82471f85d2
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-03 10:25:41 -0700

    first commit

---
+++ first_app/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>Guess The Language!</h1>'
+
+if __name__ == '__main__':
+	app.run(debug=True)
