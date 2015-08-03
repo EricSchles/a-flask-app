aa483526b5b7a80ee840da2e58ad14ba9ef12e50
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-03 11:04:54 -0700

    added new simple route

--- first_app/app.py
+++ first_app/app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def index():
 	return '<h1>Guess The Language!</h1>'
 
+@app.route('/guess/<init:id>')
+def guess(id):
+	return('<h1>Guess The Language!</h1>'
+			'<p>My guess: {0}</p>').format(guesses[id])
+
 if __name__ == '__main__':
 	app.run(debug=True)
