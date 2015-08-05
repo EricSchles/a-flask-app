1058c7f5945475e085591c8ff66151b3ddb513d2
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-05 12:09:16 -0700

    bug fixed, followed video up to 1:58:50

--- first_app/app.py
+++ first_app/app.py
@@ -3,7 +3,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import Form
 from wtforms.fields import RadioField, StringField, SubmitField
-from game import Guess
+from wtforms.validators import Required
+from game import Guess, GuessError
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -16,10 +17,9 @@ class YesNoQuestionForm(Form):
 	submit = SubmitField('Submit')
 
 class LearnForm(Form):
-	language = StringField('What language did you pick?')
-	question = StringField('What is a question that differentiates your language from mine?')
-	answer = RadioField('What is the answer for your language?', 
-						choices=[('yes','Yes'), ('no','No')])
+	language = StringField('What language did you pick?', validators=[Required()])
+	question = StringField('What is a question that differentiates your language from mine?', validators=[Required()])
+	answer = RadioField('What is the answer for your language?', choices=[('yes','Yes'), ('no','No')], validators=[Required()])
 	submit = SubmitField('Submit')
 
 @app.route('/')
@@ -37,25 +37,26 @@ def question(id):
 		return redirect(url_for('question', id=new_id))
 	return render_template('question.html', question=question, form=form)
 
-@app.route('/guess/<int:id>')
+@app.route('/guess/<int:id>', methods=['GET','POST'])
 def guess(id):
 	form = YesNoQuestionForm()
 	if form.validate_on_submit():
 		if form.answer.data == 'yes':
 			return redirect(url_for('index'))
 		return redirect(url_for('learn', id=id))
-	return render_template('guess.html', guess=game.get_guess(id))
+	return render_template('guess.html', guess=game.get_guess(id), form=form)
 
 @app.route('/learn/<int:id>', methods=['GET','POST'])
 def learn(id):
 	guess = game.get_guess(id)
 	form = LearnForm()
 	if form.validate_on_submit():
-		game.expand(guess, form.language.data, form, form.answer.data == 'yes')
+		game.expand(guess, form.language.data, form.question.data, form.answer.data == 'yes')
 		return redirect(url_for('index'))
-	return render_template('learn.html', guess=guess)
+	return render_template('learn.html', guess=guess, form=form)
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	#app.run(debug=True)
 	#run on development web server? use this
-	#app.run(host='0.0.0.0', port=5000, debug=True)
+	app.run(host='0.0.0.0', port=5000, debug=True)
